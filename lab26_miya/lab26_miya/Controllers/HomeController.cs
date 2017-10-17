298a5393a08ad01a318eefeeb10f1809bf9567a9
miyaluc <srlucas3@gmail.com>
2017-10-17 11:07:05 -0700

    lab complete

---
+++ lab26_miya/lab26_miya/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using lab26_miya.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab26_miya.Controllers
+{
+    public class HomeController : Controller
+    {
+        private lab26_miyaContext _context;
+
+        public HomeController(lab26_miyaContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var result = _context.CPS.Where(c => c.ID == 1);
+
+            return View(result.ToList());
+        }
+    }
+}
---
+++ lab26_miya/lab26_miya/Startup.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using lab26_miya.Models;
+using lab26_miya.Data;
+using Microsoft.AspNetCore.Identity;
+
+namespace lab26_miya
+{
+    public class Startup
+    {
+        public IConfiguration Configuration
+        {
+            get;
+        }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+                    services.AddDbContext<lab26_miyaContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("lab26_miyaContext")));
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("lab26_miyaContext")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseAuthentication();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
